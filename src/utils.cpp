171e7c2aeee31d09dba5b85ad1a87314157174dc
Homam <homamassr2@gmail.com>
2022-01-03 21:45:12 +0200

    Remove some unused code

--- src/utils.cpp
+++ src/utils.cpp
@@ -24,14 +24,3 @@ std::string glmVec4ToString(const glm::ivec4& v)
     return "(" + std::to_string(v.x) + ", " + std::to_string(v.y) + ", " + std::to_string(v.z) + ", " + std::to_string(v.w) + ")";
 }
 
-template <int comp, typename T, glm::qualifier T2>
-void xxx(glm::vec<comp, T, T2> s)
-{
-
-}
-
-void vv()
-{
-    xxx(glm::vec2());
-    xxx(glm::ivec2());
-}
